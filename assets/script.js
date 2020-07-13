edbdfdf9db93e52a70a3c794d31f6af032c365f9
Sarah Abouelela <abouelelas@gmail.com>
2020-07-13 15:16:09 -0400

    ajax call to five day forecast api not implementing

--- assets/script.js
+++ assets/script.js
@@ -8,8 +8,6 @@ var apiKey = "38bd3e1bcd10ebe74c740e701e06f2c5";
 // var queryUrlForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
 // var queryUrlUvindex = "https://openweathermap.org/api/uvi" + cityName + "&appid=" + apiKey;
 
-// var sampleQuery = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=" + apiKey;
-
 // function search() {
 //   $('#search-button').on('click', function () {
 //     citySearch = $('#city-input')
@@ -34,6 +32,7 @@ var apiKey = "38bd3e1bcd10ebe74c740e701e06f2c5";
 //     console.log(response);
 
 $(document).ready(function () {
+  
   $("#select-city").on("click", function (event) {
     event.preventDefault();
     var userInput = $("#city-input").val();
@@ -44,7 +43,7 @@ $(document).ready(function () {
 
     getWeather(userInput)
   });
-
+  
   $("#searchhistory").on("click", "li", function () {
     getWeather($(this).text());
   });
@@ -53,10 +52,44 @@ function searchHistory(text) {
   var li = $("<li>").addClass("search-list").text(text);
   $("#searchhistory").append(li)
 }
+function getUVIndex(lat, lon) {
+
+
+  var queryUrlUvindex = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`;
+  $.ajax({
+    url: queryUrlUvindex,
+    method: "GET",
+    success: function (UVdata) {
+      console.log(UVdata);
+
+      var UVIndex = UVdata.value;
+      // var pTag = $("#current").text("UV Index: " + UVIndex)
+      //.append(pTag);
+      // pTag.addClass("index");
+      var UVcond;
+      console.log(UVIndex);
+      // UV Index color that indicates whether the conditions are favorable, moderate, or severe
+      $("#uvl-display").text("UV Index: " + UVIndex);
+      if (UVIndex > 2 && UVIndex <= 6) {
+        $("#uvl-display").css("background-color", "#82E0AA");
+      }
+      else if (UVIndex > 6 && UVIndex < 8 ) {
+        $("#uvl-display").css("background-color", "#FFF176");
+      }
+      else if (UVIndex >= 8 && UVIndex <= 10) {
+        $("#uvl-display").css("background-color", "#FFB74D");
+      }
+      else if (UVIndex >= 11) {
+        $("#uvl-display").css("background-color", "#DA240B");
+      }
+    }
+  })
+}
+
 
 function getWeather(userInput) {
 
-  
+
   var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=" + apiKey
   $.ajax({
     url: queryUrl,
@@ -86,8 +119,9 @@ function getWeather(userInput) {
 
       var wind = $("<p>").addClass("cardText").text("Windspeed: " + data.wind.speed + " MPH");
       var humidity = $("<p>").addClass("cardText").text("Humidity: " + data.main.humidity + " %");
-
-      //var wind, humidity, name of city
+      var lat = data.coord.lat;
+      var lon = data.coord.lon;
+      //-----append to DOM-------//
       cardBody.append(imgDiv);
       card.append(cardBody)
       $("#current-weather-container").append(card);
@@ -107,115 +141,61 @@ function getWeather(userInput) {
       cardBody.append(humidity);
       card.append(cardBody)
       $("#current").append(card);
-
-   
-
-      var lat = data.coord.lat;
-      var lon = data.coord.lon;
-
-      var queryUrlUvindex = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${data.coord.lat}&lon=${data.coord.lon}`;
-      $.ajax({
-        url: queryUrlUvindex,
-        method: "GET",
-
-      }).then(function (UVdata) {
-        var UVIndex = UVdata.value;
-        var pTag = $("#current").text("UV Index: " + UVIndex)
-        city.append(pTag);
-        pTag.addClass("index");
-        var UVcond
-        console.log(UVIndex);
-        //UV Index color that indicates whether the conditions are favorable, moderate, or severe
-       
-        if (UVIndex > 2) {
-          $("#current").css("background-color", "#82E0AA");
-          UVcond = $("<p>").text("Favorable");
-          city.append(UVcond);
-        }
-        else if (UVIndex < 6) {
-          $("#current").css("background-color", "#FFF176");
-          UVcond = $("<p>").text("Moderate");
-          city.append(UVcond);
-        }
-        else if (UVIndex < 8) {
-          $("#current").css("background-color", "#FFB74D");
-          UVcond = $("<p>").text("Moderate");
-          city.append(UVcond);
-        }
-        else if (UVIndex < 11) {
-          $("#current").css("background-color", "red");
-          UVcond = $("<p>").text("Severe");
-          city.append(UVcond);
-        }
-        else {
-          $("#current").css("background-color", "firebrick");
-          UVcond = $("<p>").text("SEVERE");
-          city.append(UVcond);
-        }
-        city.append(UVIndex);
-
+      getUVIndex(lat, lon);
+     
+      // ---------5 day forecast---------------//
+      function getForecast(city) {
+      var queryUrlForecast = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityName + "&appid=" + apiKey;
+      // $.ajax({
+      //   url: queryUrlForecast,
+      //   method: "GET",
+      //   success: function (data) {
+      //     console.log(data);
+      //   }
+        
+          //get 5 day forecast
+         
+          $.ajax({
+              url: queryURL,
+              method: "GET"
+          }).then(function (response) {
+              //add container div for forecast cards
+              var newrow  = $("<p>").addClass("cardText").text( data.name + "   " + moment(data.dt_txt).format('LL'));
+            
+      
+              //loop through array response to find the forecasts for 15:00
+              var fiveTemp = day.main.temp
+    fiveTemp = Math.floor((fiveTemp - 273.15) * 1.8 + 32);
+    var fiveHumid = day.main.humidity
+    var icon = day.weather[0].icon;
+              for (var i = 0; i < 5; i++) {
+                  if (data.list[i].dt_txt.indexOf([i]) !== -1) {
+                      var newCol = $("<div>").attr("class", "one-fifth");
+                      newrow.append(newCol);
+      
+                      var newCard = $("<div>").attr("class", "card text-white bg-primary");
+                      newCol.append(newCard);
+      
+                      var cardHead = $("<div>").attr("class", "card-header").text(moment(response.list[i].dt, "X").format("MMM Do"));
+                      newCard.append(cardHead);
+      
+                      var cardImg = $("<img>").attr("class", "card-img-top").attr("src", "https://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + "@2x.png");
+                      newCard.append(cardImg);
+      
+                      var bodyDiv = $("<div>").attr("class", "card-body");
+                      newCard.append(bodyDiv);
+      
+                      bodyDiv.append($("<p>").attr("class", "card-text").html("Temp: " + data.list[i].main.temp + " &#8457;"));
+                      bodyDiv.append($("<p>").attr("class", "card-text").text("Humidity: " + data.list[i].main.humidity + "%"));
+                  }
+                }
+          });
+      }
       })
     }
-    // end call back response from AJAX
-  }); //success of AJAX call 
-      $("#current").empty();
 
-
-  // $.ajax({
-  //   url: "https://api.openweathermap.org/data/2.5/weather?q=40ea71f932558a6b52a82bff32c16d37",
-  // }).done(function(data) {
-  //   $('#city-input').append($("<p>").addClass("cardText").text("Temp: " + data.main.temp + " °F"));
-  // });
-
-  //   //this is where you should have code to show on html 
-  //   var card = $("<div>").addClass("card");
-  //   var cardBody = $("<div>").addClass("#current-weather-container");
-  //   var temperature = $("<p>").addClass("cardText").text("Temp: " + data.main.temp + " °F");
-  //   var wind = $("<p>").addClass("cardText").text("Temp: " + data.main.windspeed + " °F");
-  //   var humidity = $("<p>").addClass("cardText").text("Temp: " + data.main.humidity + " °F");
-  //   var city = $("<p>").addClass("cardText").text("Temp: " + data.main.temp + " °F");
-  //   //var wind, humidity, name of city
-
-  //   cardBody.append("#temperature");
-  //   card.append(cardBody)
-  //   $("#current-weather-container").append(card);
-
-  //   //call function to get forecast (UserInput)
-  //   //call function for UV
-  // }
-  // //   $.ajax({
-  // //     url: queryUrl,
-  // //     cityName,
-  // //     method: 'GET',
-
-  // // });
-  // // // console.log(queryUrl);
-  // console.log(cityName);
-
-  //var userStorage = JSON.parse(window.localStorage.getItem("userStorage"))
-  // if (userStorage.length > 0) {
-  //getWeather(userStorage[userStorage.length-1])
-  //}
-
-  //for loop {searchHistory(userStorage[i])}
-
-
-}// ------ end getWeather fct def ------
-
-    // })
-  // }
-// })
-// console.log(event.target)
-    // moment().format(mm.dd.yyyy);
-
-  // })
-
-//     }).then(function (response) {
-//         console.log(response);
-//         console.log(queryURL);
-//         //empty divs and ids that we need to dump content into.....
-//         $("#current").empty();
-//        var mainDate = moment().format('L');
-    // });
-      // });
-    // 
+// -------Empty div contents for next user input---------//
+$("#select-city").on("click", function (event) {
+  event.preventDefault();
+  $("#current").empty();
+});
